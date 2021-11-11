77b6e0881c0cf8f3655d579717ac4511a15deeb1
Manuel Gutiérrez <89094000+GutierManu@users.noreply.github.com>
2021-11-11 13:04:19 -0600

    Title Screen Script
    -Edited the MainMenu script
    -Assigned the pertinent methods to each button on the Title Screen

--- Programming Theory Project/Assets/Scripts/MainMenu.cs
+++ Programming Theory Project/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
